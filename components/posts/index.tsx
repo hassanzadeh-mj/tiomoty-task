0bf5811ea403ab6ee350e430602a30122f2ebe34
Netbar <m.hassanzadeh@netbar.ir>
2023-08-15 15:09:48 +0330

    add : utils

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -1,7 +1,6 @@
 import {message} from "antd";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
-import {GetAllPosts} from "@/services/api/post";
 interface IPostsResponse {
 
 }
@@ -27,18 +26,6 @@ const Posts = () => {
     function search() {
         setLoading(true)
 
-        GetAllPosts().then(async data => {
-            setLoading(false)
-
-            if (data.successful) {
-                setPosts(prv => data.content ?? prv)
-            }
-            else {
-                messageApi.error({
-                    content: data.title.optional('امکان نمایش پست ها وجود ندارد')
-                })
-            }
-        })
 
     }
     return(
---
+++ utils/post.ts
@@ -0,0 +1,15 @@
+
+export interface IPostsResponse {
+    id:	number
+    slug:string
+    url	:string
+    title : string
+    content	:string
+    image :	string
+    thumbnail:string
+    status:string
+    category:string
+    publishedAt	:string
+    updatedAt:string
+    userId:number
+}
